b6ae0aac0b195128ffdbfea75adee39b8bcc7112
MiguelCostaA <149885440+MiguelCostaA@users.noreply.github.com>
2023-12-05 13:52:25 +0000

    Add files via upload

---
+++ ClientRESTServer (1).zip_expanded/ClientRESTServer/src/main/java/cenas/ClientTestHttpURLConnection.java
@@ -0,0 +1,767 @@
+package cenas;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
+
+import com.google.gson.Gson;
+
+
+import upt.Tarefa;
+import upt.Categoria;
+import upt.Estado;
+import upt.Recorrencia;
+import upt.Utilizador;
+
+public class ClientTestHttpURLConnection {
+	
+	public static void main(String[] args) {
+		
+		addTarefa();
+		getTarefa();
+		updateTarefa();
+		
+		addCategoria();
+		getCategoria();
+		updateCategoria();
+		
+		addEstado();
+		getEstado();
+		updateEstado();
+		
+		addRecorrencia();
+		getRecorrencia();
+		updateRecorrencia();
+		
+		addUtilizador();
+		getUtilizador();
+		updateUtilizador();
+		
+	}
+	/**
+     * Método para adicionar uma nova Tarefa.
+     */
+	private static void addTarefa() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+		
+		try {
+			URL url = new URL("http://localhost:8080/ProjetoREST/tarefa/addTarefa");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			String postData = gson.toJson(new Tarefa("Limpar o quarto", "Arrumar a cama, limpar a secretária", "19/04/2024", "Alta"), Tarefa.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getTarefa();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+
+		}
+		
+	}
+	/**
+     * Método para atualizar uma Tarefa existente.
+     */
+	private static void updateTarefa() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/tarefa/updateTarefa");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("PUT");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+
+			String postData = gson.toJson(new Tarefa("Limpar o quarto", "Arrumar a cama, limpar a secretária", "19/04/2024", "Alta"), Tarefa.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getTarefa();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+     * Método para obter informações sobre todas as Tarefas.
+     */
+	private static void getTarefa() {
+		HttpURLConnection conn = null;
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/tarefa/getTarefa");
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.connect();
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			Gson gson = new Gson();
+			List<Tarefa> tarefas = Arrays.asList(gson.fromJson(br, Tarefa[].class));
+			System.out.println("Output JSON from Server .... \n");
+			for (Tarefa t : tarefas) {
+				System.out.println(t);
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para adicionar uma nova Categoria.
+	 */
+	private static void addCategoria() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+		
+		try {
+			URL url = new URL("http://localhost:8080/ProjetoREST/categoria/addCategoria");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			String postData = gson.toJson(new Categoria("Pessoal"), Categoria.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getCategoria();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+
+		}
+	}
+	/**
+	 * Método para atualizar uma Categoria existente.
+	 */
+	private static void updateCategoria() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/categoria/updateCategoria");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("PUT");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+
+			String postData = gson.toJson(new Categoria("Pessoal"), Categoria.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getCategoria();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para obter informações sobre todas as Categorias.
+	 */
+	private static void getCategoria() {
+		HttpURLConnection conn = null;
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/categoria/getCategoria");
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.connect();
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			Gson gson = new Gson();
+			List<Categoria> categorias = Arrays.asList(gson.fromJson(br, Categoria[].class));
+			System.out.println("Output JSON from Server .... \n");
+			for (Categoria c : categorias) {
+				System.out.println(c);
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para adicionar um novo Estado.
+	 */
+	private static void addEstado() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+		
+		try {
+			URL url = new URL("http://localhost:8080/ProjetoREST/estado/addEstado");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			String postData = gson.toJson(new Estado("Concluído"), Estado.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getEstado();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para atualizar um Estado existente.
+	 */
+	private static void updateEstado() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/estado/updateEstado");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("PUT");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+
+			String postData = gson.toJson(new Estado("Concluído"), Estado.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getEstado();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para obter informações sobre todos os Estados.
+	 */
+	private static void getEstado() {
+		HttpURLConnection conn = null;
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/estado/getEstado");
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.connect();
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			Gson gson = new Gson();
+			List<Estado> estados = Arrays.asList(gson.fromJson(br, Estado[].class));
+			System.out.println("Output JSON from Server .... \n");
+			for (Estado e : estados) {
+				System.out.println(e);
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para adicionar uma nova Recorrência.
+	 */
+	private static void addRecorrencia() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+		
+		try {
+			URL url = new URL("http://localhost:8080/ProjetoREST/recorrencia/addRecorrencia");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			String postData = gson.toJson(new Recorrencia("Semanal"), Recorrencia.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getRecorrencia();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para atualizar uma Recorrência existente.
+	 */
+	private static void updateRecorrencia() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/recorrencia/updateRecorrencia");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("PUT");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+
+			String postData = gson.toJson(new Recorrencia("Semanal"), Recorrencia.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getRecorrencia();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para obter informações sobre todas as Recorrências.
+	 */
+	private static void getRecorrencia() {
+		HttpURLConnection conn = null;
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/recorrencia/getRecorrencia");
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.connect();
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			Gson gson = new Gson();
+			List<Recorrencia> recorrencias = Arrays.asList(gson.fromJson(br, Recorrencia[].class));
+			System.out.println("Output JSON from Server .... \n");
+			for (Recorrencia r : recorrencias) {
+				System.out.println(r);
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para adicionar um novo Utilizador.
+	 */
+	private static void addUtilizador() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+		
+		try {
+			URL url = new URL("http://localhost:8080/ProjetoREST/utilizador/addUtilizador");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("POST");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+			String postData = gson.toJson(new Utilizador("Carlinhos Pereira", "carlitos123@gmail.com", "bananinha1234"), Utilizador.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getUtilizador();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+
+		}
+		
+	}
+	/**
+	 * Método para atualizar um Utilizador existente.
+	 */
+	private static void updateUtilizador() {
+		HttpURLConnection conn = null;
+		Gson gson = new Gson();
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/tarefa/updateTarefa");
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("PUT");
+
+			con.setRequestProperty("Content-Type", "application/json");
+			con.setRequestProperty("Accept", "application/json");
+
+			con.setDoOutput(true);
+			con.setDoInput(true);
+
+			String postData = gson.toJson(new Utilizador("Carlinhos Pereira", "carlitos123@gmail.com", "bananinha1234"), Utilizador.class);
+
+			DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+			wr.writeBytes(postData);
+			wr.flush();
+			wr.close();
+
+			if (con.getResponseCode() < 200 && con.getResponseCode() > 299) {
+				throw new RuntimeException("Failed : HTTP error code : " + con.getResponseCode());
+			} else {
+				getUtilizador();
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	/**
+	 * Método para obter informações sobre todos os Utilizadores.
+	 */
+	private static void getUtilizador() {
+		HttpURLConnection conn = null;
+
+		try {
+
+			URL url = new URL("http://localhost:8080/ProjetoREST/utilizador/getUtilizador");
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			conn.connect();
+
+			if (conn.getResponseCode() != 200) {
+				throw new RuntimeException("Failed : HTTP error code : " + conn.getResponseCode());
+			}
+
+			BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+			Gson gson = new Gson();
+			List<Utilizador> utilizadores = Arrays.asList(gson.fromJson(br, Utilizador[].class));
+			System.out.println("Output JSON from Server .... \n");
+			for (Utilizador u : utilizadores) {
+				System.out.println(u);
+			}
+
+		} catch (MalformedURLException e) {
+
+			e.printStackTrace();
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+			if (conn != null) {
+				try {
+					conn.disconnect();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+}
